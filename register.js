059cfbaef6d594b932901d5384adc9c2433fe4f7
dddfatehgarhsahib-hash <dddfatehgarhsahib@gmail.com>
2026-03-10 14:51:12 +0530

    Create Register component for online registration

---
+++ register.js
@@ -0,0 +1,17 @@
+import Form from "../components/Form"
+
+export default function Register(){
+
+return(
+
+<div style={{textAlign:"center",padding:"40px"}}>
+
+<h2>Online Registration</h2>
+
+<Form/>
+
+</div>
+
+)
+
+}
